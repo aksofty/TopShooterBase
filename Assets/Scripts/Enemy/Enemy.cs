2500d8a1f5e4947c9c77b90897af006a6beba9b5
aksofty <aksofty@yandex.ru>
2025-12-15 18:03:29 +0700

    Enemy Rotate Towards Player

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -8,51 +8,28 @@ public class Enemy : MonoBehaviour, IEnemy
     [SerializeField] private float detectionDistance = 5f;
     [SerializeField] private float catchDistance = 0.1f;
     [SerializeField] private float movingSpeed = 1f;
+    [SerializeField] private float rotationSpeed = 5f;
 
     private Rigidbody2D _rb;
     private float _currentPlayerDistance;
-
     private EnemyState _currentState;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _currentPlayerDistance = DistanceToPlayer();
-        
+
         _currentState = new IdleState(this);
         _currentState.Enter();
     }
 
     private void FixedUpdate()
     {
-
         _currentState?.FixedUpdate();
-
-        /*if (player == null)
-        {
-            return;
-        }
-
-        float distanceToPlayer = math.abs(
-            Vector2.Distance(transform.position, player.position));
-
-        //Debug.Log(distanceToPlayer);
-
-        if (distanceToPlayer < detectionDistance && _active == true)
-        {
-            MoveToPlayer();
-        }
-        else if (distanceToPlayer < catchDistance && _active == true)
-        {
-            _active = false;
-        }
-        else if (_active == false)
-        {
-            return;
-        }*/
     }
 
-    private void Update() {
+    private void Update()
+    {
         _currentPlayerDistance = DistanceToPlayer();
         _currentState.Update();
     }
@@ -61,10 +38,18 @@ public class Enemy : MonoBehaviour, IEnemy
     {
         Vector2 direction = ((Vector2)player.position - (Vector2)transform.position).normalized;
         _rb.linearVelocity = direction * movingSpeed;
+        RotateTo(direction);
     }
 
-    private float DistanceToPlayer() => Vector2.Distance(transform.position, player.position);        
-    
+    private void RotateTo(Vector2 direction)
+    {
+        float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        _rb.MoveRotation(targetAngle - 90f);
+    }
+
+    private Vector2 PlayerDirection() => (player.position - transform.position).normalized;
+    private float DistanceToPlayer() => Vector2.Distance(transform.position, player.position);
+
 
     public void ChangeState(EnemyState newState)
     {
@@ -72,15 +57,15 @@ public class Enemy : MonoBehaviour, IEnemy
         _currentState = newState;
         _currentState.Enter();
     }
-    
-  
+
+
     public Boolean PlayerDetected()
     {
-        if(_currentPlayerDistance < detectionDistance)
+        if (_currentPlayerDistance < detectionDistance)
         {
             return true;
         }
 
-        return false;    
+        return false;
     }
 }
