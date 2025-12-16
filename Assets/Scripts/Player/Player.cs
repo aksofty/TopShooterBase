68ed3f48231270487143a365ba2a193d7f37083d
aksofty <aksofty@yandex.ru>
2025-12-16 20:22:03 +0700

    Playground gange added

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,12 +9,14 @@ public class Player : MonoBehaviour
     [SerializeField] private float movingSpeed = 5f;
     [SerializeField] private float rotationSpeed = 5f;
 
+    [SerializeField] private float playgroundWidth = 4f;
+    [SerializeField] private float playgroundHeight = 6f;
+
     private float _rotateInput;
     private float _moveInput;
     private Rigidbody2D _rb;
-    private Boolean _paused = false;
 
-     private void Awake()
+    private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
@@ -33,11 +35,30 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+
         _rb.MoveRotation(
             _rb.rotation - _rotateInput * rotationSpeed);
 
         Vector2 moveVector = transform.up * _moveInput;
         _rb.linearVelocity = moveVector * movingSpeed;
+
+
+
+        float xPosition = Mathf.Clamp(transform.position.x, -playgroundWidth / 2, playgroundWidth / 2);
+        float yPosition = Mathf.Clamp(transform.position.y, -playgroundHeight / 2, playgroundHeight / 2);
+
+        if (transform.position.x != xPosition || transform.position.y != yPosition)
+        {
+            transform.position = new Vector2(xPosition, yPosition);
+            Debug.Log("wall");
+        }
+
+
+        /*transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x, -playgroundWidth / 2, playgroundWidth / 2),
+            Mathf.Clamp(transform.position.y, -playgroundHeight / 2, playgroundHeight / 2));*/
+
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
